3d06868eb66aed3bbee7994e9413cfb54f200a60
A.Dovgan <a.dovgan@devcaz.com>
2022-07-13 18:46:35 +0200

    firsr day

---
+++ 001_lesson_js/js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//a = 15;
+//console.log(a);
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 15;
+
+const obj = {
+    a: 50
+};
+
+obj.a = 25;
+
+console.log(name);
+
+var name = 'Art';
+
+{
+    let resultLet = 'Let';
+    var resultVar = 'Var';
+
+    console.log(resultLet);
+}
+
+console.log(resultVar);
