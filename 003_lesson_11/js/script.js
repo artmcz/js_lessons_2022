ae8841545b6d725614197fbd09ad82cdfc241b5a
A.Dovgan <a.dovgan@devcaz.com>
2022-07-14 17:54:50 +0200

    my lessons

---
+++ 003_lesson_11/js/script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+// Lesson 14 Git control version
+
+
+
+
+
+// Lesson 13 Operators in JS
+
+// console.log(4 + '5');
+// console.log(4 + +'5');
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(--incr);
+// console.log(++decr);
+
+// console.log(incr--);
+// console.log(decr++);
+
+// console.log(2*4 == 8);
+// console.log(2*4 == '8');
+// console.log(2*4 === '8');
+
+// const   isChecked = true,
+//         isClose = false;
+
+// console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
+
+// console.log(2 + 2 * 2 === 8);
+// console.log(2 + 2 * 2 != 8);
+// console.log(2 + 2 * 2 !== '6');
+
+
+
+
+// Lesson 12
+
+// const category = 'toys';
+
+// console.log(`https://artm.cz/${category}/5`);
+
+// const user = 'Art';
+
+// alert(`Привет, ${user}`);
+
+
+// Lesson 11
+
+// const answer = +prompt('Вам есть 18?', '18');
+
+// console.log(typeof(answer));
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Name?', '');
+// answers[1] = prompt('Second name?', '');
+// answers[2] = prompt('Age?', '');
+
+
+// document.write(answers);
