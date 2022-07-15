43f3b7441b09261457107bf7f2899627ee8f1338
A.Dovgan <a.dovgan@devcaz.com>
2022-07-15 17:45:38 +0200

    my lessons

--- 004_lesson_17_new_project/js/script.js
+++ 004_lesson_17_new_project/js/script.js
@@ -10,12 +10,36 @@ const personalMovieDB = {
     privat: false
 };
 
-const   a = prompt('Last film?', ''),
-        b = prompt('Rating?', ''),
-        c = prompt('Last film?', ''),
-        d = prompt('Rating?', '');
+// const   a = prompt('Last film?', ''),
+//         b = prompt('Rating?', ''),
+//         c = prompt('Last film?', ''),
+//         d = prompt('Rating?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 1; i++) {
+    const   a = prompt('Last film?', ''),
+            b = prompt('Rating?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('few movies watched');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('you are a great viewer');
+} else if (personalMovieDB.count >= 30) {
+    console.log('you are a cinephile');
+} else {
+    console.log('error');
+}
 
 console.log(personalMovieDB);
+
