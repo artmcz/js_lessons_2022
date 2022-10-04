417db972b6eaaef3551fc1cc52defeec242fc8f5
A.Dovgan <a.dovgan@devcaz.com>
2022-10-04 15:16:33 +0200

    state and lifecycle methods

--- 013_lesson_components_props/src/components/app/app.js
+++ 013_lesson_components_props/src/components/app/app.js
@@ -1,8 +1,10 @@
+import Clock from "../clock/clock";
 import Welcome from "../welcome/welcome";
 
 function App() {
   return (
     <div>
+      <Clock /><Clock /><Clock /><Clock />
       <Welcome name="Алиса" age='22' testDef = 'ff' />
       <Welcome name="Базилио"  age='18' testDef = 'aa' />
       <Welcome name="Буратино"  age='71' testDef = 'dd' />
