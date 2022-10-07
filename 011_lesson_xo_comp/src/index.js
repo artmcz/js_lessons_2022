349350c79518b6d2f7b65e961930faa777090bf1
A.Dovgan <a.dovgan@devcaz.com>
2022-10-07 18:54:29 +0200

    class to func component

---
+++ 011_lesson_xo_comp/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import Game from './components/game/game';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>
+);
+
+
+
+
